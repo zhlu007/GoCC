74bfff2fbcb8283d7665db97d0fe3cb12402e2fb
zhlu007 <48824075+zhlu007@users.noreply.github.com>
2021-05-25 21:29:32 +0800

    Update main.m

--- main.m
+++ main.m
@@ -3,35 +3,13 @@ clc; close all; clear;
 
 for times = 1:1  %runing times
 
-A=importdata('Gowalla_edges.txt','\t'); %input the original dataset
+    original_data = importdata('Gowalla_edges.txt','\t');
 
-B=A.data;
+    OrData = original_data.data;
 
-[m,n]=size(B);
-
-max(B);
-
-min(B);
-
-C1 = unique(B(:,2));
-
-C2 = unique(B(:,1));
-
-C=unique([C1 ;C2]);
-
-max(C);
-min(C);
-[mm,nn]=size(C);
-network=zeros(max(C),max(C));
-temp1=0;
-temp2=0;
-for i=1:m
-    temp1=B(i,1);
-    temp2=B(i,2);
-    network(temp1,temp2)=1;    
-end 
-
-fname = cellstr(ls('nodes_*.csv')); % randomly select users from the original dataset
+    OrData = OrData + 1;
+    
+% fname = cellstr(ls('nodes_*.csv')); 
 
 iname={'Task Coverage', 'Number of Recruited Users', 'Total Social Cost','Average Pairwise Communication Cost','Running Times (s)'};
 
@@ -49,19 +27,19 @@ XLabel_number={connected_users user_scale task_scale cooperative_scale};
 
 num_edge=[length(connected_users) 1 1 1];
 
-num_user=[1 length(fname) 1 1];
+num_user=[1 length(user_scale) 1 1];
 
 num_task=[1 1 length(task_scale) 1];
 
 num_cooperation=[1 1 1 length(cooperative_scale)];
 
-for impact_index=1:length(impact_name)
+for impact_index=1 : length(impact_name)
     
-    MCT_M=[];
-    
-    MCT_S=[];
-    
-    C_C=[];
+%     MCT_M=[];
+%     
+%     MCT_S=[];
+%     
+%     C_C=[];
     
     GoCS_G=[];
     
@@ -69,39 +47,60 @@ for impact_index=1:length(impact_name)
     
 %%%%%%%%%%%%%%%%%%%%%%%% user scale setting %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    for name_index=1:num_user(impact_index)
+    for name_index = 1 : num_user(impact_index)
 
-         f_name=name_index;
+         f_name = name_index;
         
         if impact_index ~= 2
             
-            f_name=4; % the number of users : 600 
+            f_name = 4; % the number of users : 600 
             
         end
         
-        M = csvread(fname{f_name});
-
-        temp=network(:,M);%obtain users' relationship 
-
-        A=temp(M,:);%obtain users' relationship 
+        users = str2num (user_scale{f_name});
         
-         graph_network= A;
-%         
-         graph_network=(graph_network~=0);
-         
-         graph_network(sum(graph_network,1)<5,:)=0;
+        experimental_data =  randperm(users);%% randomly select users from the original dataset
 
-        [users,n]=size(graph_network); 
+        ExpData = zeros(users, users);
+
+        for i = 1 : users
+
+            experimental_data(i);
+
+           temp = OrData(find(OrData(:,1) == experimental_data(i)),2);
+
+           joint_set = intersect(temp', experimental_data);
+
+           for j = 1 : length(joint_set)
+
+               index = find(experimental_data == joint_set(j));
+
+               ExpData (i, index) = 1;
+
+           end
+        end
+
+        ExpData = ExpData + ExpData';
+
+        UserUser = (ExpData ~= 0);
+        
+        graph_network = UserUser;
+        
+        graph_network=(graph_network ~= 0);
+         
+        graph_network(sum(graph_network, 1) < 2, :)=0;
+
+        [users, n] = size(graph_network); 
         
         Rgraph_network=graph_network;
         
  %%%%%%%%%%%%%%%%%%%%%%%% community size setting %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
-        for d=1:num_edge(impact_index)
+        for d = 1 : num_edge(impact_index)
             
-            if impact_index ~=1
+            if impact_index ~= 1
                 
-                d=3; % community size:[5, 30]
+                d = 3; % community size:[5, 30]
                 
             end
             
@@ -129,7 +128,7 @@ for impact_index=1:length(impact_name)
                     
                     node_low = node_up + 1;
 
-                    nodes = randi(node_range,1,1);
+                    nodes = randi(node_range, 1, 1);
 
                     node_up = nodes + node_low - 1;
                     
@@ -137,7 +136,7 @@ for impact_index=1:length(impact_name)
                         
                         for v = node_low : node_up
                         
-                            random_graph(k,v) = Rgraph_network(rank_user(k),rank_user(v));
+                            random_graph(k, v) = Rgraph_network(rank_user(k), rank_user(v));
                         
                         end
                     end            
@@ -152,7 +151,7 @@ for impact_index=1:length(impact_name)
                         
                         for v = node_low : node_up
                         
-                            random_graph(k,v) = Rgraph_network(rank_user(k),rank_user(v));
+                            random_graph(k, v) = Rgraph_network(rank_user(k), rank_user(v));
                         
                         end
                     end
@@ -161,23 +160,23 @@ for impact_index=1:length(impact_name)
             
             end
             
-           graph_network = random_graph + random_graph';
+            graph_network = random_graph + random_graph';
            
-            graph_network = (graph_network~=0);
+            graph_network = (graph_network ~= 0);
    
-            max(sum(graph_network,2));
+            max(sum(graph_network, 2));
 
-            min(sum(graph_network,2));
+            min(sum(graph_network, 2));
 
-            min(sum(graph_network,1));
+            min(sum(graph_network, 1));
 
-            length(find(sum(graph_network~=0)~=0));
+            length(find(sum(graph_network ~= 0) ~= 0));
 
-            sum(sum(graph_network~=0));
+            sum(sum(graph_network ~= 0));
 
             Graph = graph_network;
 
-            neighbor = (graph_network~=0);
+            neighbor = (graph_network ~= 0);
 
             G = graph(graph_network);
     %  
@@ -189,7 +188,7 @@ for impact_index=1:length(impact_name)
 
             for t = 1 : max(bins)
 
-                num_bins(t) = length(find(bins==t));
+                num_bins(t) = length(find(bins == t));
 
             end
 
@@ -207,25 +206,23 @@ for impact_index=1:length(impact_name)
 
                 tasks = str2num(task_scale{t});
 
-                users = users;
+                sensing_tasks = sort(randperm(tasks, tasks));%the total number of sensing tasks varies from 10 to 50
 
-                sensing_tasks = sort(randperm(tasks,tasks));%the total number of sensing tasks varies from 10 to 50
-
-                uncovered_sensing_tasks = (sensing_tasks~=0);
+                uncovered_sensing_tasks = (sensing_tasks ~= 0);
 
                 user_single_bid = unifrnd (5, 10, 1, users);% the cost of each bid is uniformly distributed in [5,10]
 
                  user_multi_bid = unifrnd (5, 10, users, tasks);
 
-                user_task_set = zeros(users,tasks);%the user-task pairs matrix
+                user_task_set = zeros(users, tasks);%the user-task pairs matrix
 
-                num = randi([3,5],1,users);%the number of biding tasks of each user are uniformly distributed in [3,5]
+                num = randi([3, 5], 1, users);%the number of biding tasks of each user are uniformly distributed in [3,5]
 
                 for i = 1 : users 
 
-                    index = randperm(tasks,num(i));
+                    index = randperm(tasks, num(i));
 
-                    user_task_set(i,index) = 1;
+                    user_task_set(i, index) = 1;
 
                 end
 
@@ -253,7 +250,7 @@ for impact_index=1:length(impact_name)
                         
                     end
 
-                    required_cooperative_index = randi(str2num(cooperative_scale{cindex}),1,tasks);%the default value of cooperative index is uniformly distributed in [2,5]
+                    required_cooperative_index = randi(str2num(cooperative_scale{cindex}), 1, tasks);%the default value of cooperative index is uniformly distributed in [2,5]
               
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% GoCC Algorithm %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
                     beta = 0.5;
@@ -265,31 +262,31 @@ for impact_index=1:length(impact_name)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% initial leader selection %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                     t1 = clock;
                     
-                    seed = Leader_Selector(neighbor,neighbor,K);% initial leader selection 
+                    seed = Leader_Selector(neighbor, neighbor, K);% initial leader selection 
 
  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% candidate group formation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                    group = Group_Formation(seed,tasks,users,neighbor,required_cooperative_index,user_task_set,user_single_bid,communication_cost,beta); 
+                    group = Group_Formation(seed, tasks, users, neighbor, required_cooperative_index, user_task_set, user_single_bid, communication_cost, beta); 
                     
                     t2 = clock;
                     
-                    GOCC2_running_time = etime(t2,t1)
+                    GOCC2_running_time = etime(t2,t1);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% primary group selelction and cooperative group selection %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                    GoCS_result = Group_based_Cooperator_Selection(group,seed,tasks,required_cooperative_index,user_task_set,user_single_bid,communication_cost,gamma,beta);
+                    GoCS_result = Group_based_Cooperator_Selection(group, seed, tasks, required_cooperative_index, user_task_set, user_single_bid, communication_cost,gamma,beta);
 
                     GoCS_result(:,end) = GoCS_result(:,end) + GOCC2_running_time;           
                     
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Results %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+%                     MCT_M=[MCT_M; TMC_result(1,:)];
+% 
+%                     MCT_S=[MCT_S; TMC_result(2,:)];
+% 
+%                     C_C= [C_C; C2_result];
 
-                    MCT_M=[MCT_M; TMC_result(1,:)];
+                    GoCS_G = [GoCS_G; GoCS_result(1,:)];
 
-                    MCT_S=[MCT_S; TMC_result(2,:)];
-
-                    C_C= [C_C; C2_result];
-
-                    GoCS_G=[GoCS_G; GoCS_result(1,:)];
-
-                    GoCS_U=[GoCS_U; GoCS_result(2,:)];
+                    GoCS_U = [GoCS_U; GoCS_result(2,:)];
 
                 end
 
@@ -300,12 +297,12 @@ for impact_index=1:length(impact_name)
     end
 
  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% output results %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-MCT_M 
-
-MCT_S
-
-C_C
+% 
+% MCT_M 
+% 
+% MCT_S
+% 
+% C_C
 
 GoCS_G
 
